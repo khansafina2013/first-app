cd1da46de6c615d95a2f6c0299ddc8a5e87842d0
khansafina2013 <khansafina2013@gmail.com>
2024-08-09 22:18:51 +0000

    tesr

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,44 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write(
-    "Safina Khan."
-)
+    "Safina Khan.")   
+
+
+first_number = st.text_input("Type a number 10-20:point_down:")
+second_number = st.text_input("Type a number 50-70:point_down:")
+
+number_sum = int(first_number) + int(second_number)
+
+st.write(str(number_sum))
+
+#display the sum of number 1 and 2
+st.write(str(first_number)  + "+" + str(second_number)  
+         + "=" + str(number_sum))
+
+
+# write an if elif else statment for the first number 
+
+if int(first_number) < 10:
+    st.warning("first number is too low")
+
+elif int(first_number) > 20:
+    st.warning("first number is too high")
+
+else:
+    st.success("number " + str(first_number) + " is okay")    
+
+# write an if elif else statment for the second number
+
+
+if int(second_number) < 50:
+    st.warning("second number is too low")
+
+elif int(second_number) > 70:
+    st.warning("second number is too high")  
+
+else:
+    st.success("number " + str(second_number) + " is okay")
+
+  
 
 
